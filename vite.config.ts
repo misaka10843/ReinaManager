98b0702a9c9bbc6ac52f34d7ecd232485a0c27c0
misaka10843 <misaka10843@outlook.jp>
2026-08-06 13:12:03 +0800

    fix: tauri busy or locked error caused by Vite watch

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
 		// Tauri 工作于固定端口，如果端口不可用则报错
 		strictPort: true,
 		port: 5173,
+		watch: {
+      		ignored: [
+      		  "**/src-tauri/**"
+      		]
+    	}
 	},
 	clearScreen: false,
 	// 添加有关当前构建目标的额外前缀，使这些 CLI 设置的 Tauri 环境变量可以在客户端代码中访问
